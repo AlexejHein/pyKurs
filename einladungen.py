b4252940bff3a81021673ea369114c7f4355d144
Alexej Hein <alexej.hein@aol.de>
2024-03-05 07:20:26 +0100

    commit

--- einladungen.py
+++ einladungen.py
@@ -6,5 +6,6 @@ desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 for i, name in enumerate(namen):
     file_path = os.path.join(desktop_path, f'Einladung_{i+1}.txt')
     with open(file_path, 'w') as f:
+        f.write("Einladung zur Party\n")
         f.write("Hallo " + name + ", ich lade dich zu meiner Party ein.\n")
         f.write("Ich freue mich auf dein Kommen.\n")
