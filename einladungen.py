79a053dc78f29a321a029c752dd7f33bceea1c9f
Alexej Hein <alexej.hein@aol.de>
2024-03-05 06:25:45 +0100

    commit

---
+++ einladungen.py
@@ -0,0 +1,5 @@
+namen = ["Hans", "Peter", "Klaus", "Maria", "Anna", "Eva", "Lena", "Lisa"]
+
+for name in namen:
+    print("Hallo " + name + ", ich lade dich zu meiner Party ein.")
+    print("Ich freue mich auf dein Kommen.\n")
