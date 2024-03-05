4b2919717c95048fbedc4ecaf5647cc8396c2fe7
Alexej Hein <alexej.hein@aol.de>
2024-03-05 06:57:36 +0100

    commit

--- einladungen.py
+++ einladungen.py
@@ -1,5 +1,10 @@
-namen = ["Hans", "Peter", "Klaus", "Maria", "Anna", "Eva", "Lena", "Lisa"]
+import os
 
-for name in namen:
-    print("Hallo " + name + ", ich lade dich zu meiner Party ein.")
-    print("Ich freue mich auf dein Kommen.\n")
+namen = ["Hans Peter", "Peter Hans", "Klaus Müller"]
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
+for i, name in enumerate(namen):
+    file_path = os.path.join(desktop_path, f'Einladung_{i+1}.txt')
+    with open(file_path, 'w') as f:
+        f.write("Hallo " + name + ", ich lade dich zu meiner Party ein.\n")
+        f.write("Ich freue mich auf dein Kommen.\n")
