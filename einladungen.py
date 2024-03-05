a9f61993e58fee4f6e32ce105d58812676aa9645
Alexej Hein <alexej.hein@aol.de>
2024-03-05 07:37:29 +0100

    commit

--- einladungen.py
+++ einladungen.py
@@ -1,7 +1,17 @@
 import os
 
-namen = ["Hans Peter", "Peter Hans", "Klaus Müller"]
+# Definieren von 'desktop_path' vor der Verwendung
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
+# Pfad zur Textdatei
+file_path = os.path.join(desktop_path, 'namen.txt')
+
+# Öffnen der Datei und Lesen der Zeilen
+with open(file_path, 'r') as f:
+    lines = f.readlines()
+
+# Entfernen des Zeilenumbruchs am Ende jeder Zeile und Hinzufügen zur Liste 'namen'
+namen = [line.strip() for line in lines]
 
 for i, name in enumerate(namen):
     file_path = os.path.join(desktop_path, f'Einladung_{i+1}.txt')
